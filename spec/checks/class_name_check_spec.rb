5fde808367621426308d9cfb5e78be7af99fa970
Marty Andrews <marty@martyandrews.net>
2008-08-24 22:23:44 +1000

    moved to match lib folder.

--- spec/checks/class_name_check_spec.rb
+++ spec/checks/class_name_check_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require 'roodi'
 require 'checks/class_name_check'
 
