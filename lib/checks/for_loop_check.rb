7fee4df775fd4b3ec280bb14430db77c68842112
Marty Andrews <marty@martyandrews.net>
2008-08-25 18:44:09 +1000

    [NEW] for loop check and corresponding spec.

---
+++ lib/checks/for_loop_check.rb
@@ -0,0 +1,13 @@
+require 'java'
+require 'checks/check'
+include_class 'org.jruby.ast.ForNode'
+
+class ForLoopCheck < Check
+  def interesting_nodes
+    [ForNode]
+  end
+
+  def evaluate(node)
+    add_error "#{position(node)} - Don't use 'for' loops. Use Enumerable.each instead."
+  end
+end
---
+++ spec/checks/for_loop_check_spec.rb
@@ -0,0 +1,20 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+require 'roodi'
+require 'checks/for_loop_check'
+
+describe ForLoopCheck do
+  before(:each) do
+    @roodi = Roodi.new(ForLoopCheck.new)
+  end
+  
+  it "should reject for loops" do
+    content = <<-END
+    for i in 1..2
+    end
+    END
+    errors = @roodi.check_content(content)
+    errors.should_not be_empty
+    errors[0].should eql("dummy-file.rb:1 - Don't use 'for' loops. Use Enumerable.each instead.")
+  end
+end
