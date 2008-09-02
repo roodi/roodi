1db2a0d72b64270a6b33aad079ba289c8d1b43e9
Marty Andrews <marty@martyandrews.net>
2008-09-02 15:57:46 +1000

    added support for specifying a config file. added support for passing in multiple patterns to script.

--- lib/roodi/core/runner.rb
+++ lib/roodi/core/runner.rb
@@ -8,17 +8,26 @@ include_class 'org.jruby.RubyInstanceConfig'
 module Roodi
   module Core
     class Runner
+      DEFAULT_CONFIG = File.join(File.dirname(__FILE__), "..", "..", "..", "roodi.yml")
+      
       def initialize(*checks)
-        @checks = checks.empty? ? load_checks : checks
+        @config = DEFAULT_CONFIG
+        @checks = checks unless checks.empty?
         @runtime = Ruby.newInstance(RubyInstanceConfig.new)
-        checking_visitor = CheckingVisitor.new
-        checking_visitor.checks = @checks
-        @iterator_visitor = IteratorVisitor.new(checking_visitor)
+      end
+      
+      def config=(config)
+        @config = config
+        @checks = nil
       end
       
       def check(filename, content)
+        @checks ||= load_checks
         node = @runtime.parse(content, filename, nil, 0, false)
-        node.accept(@iterator_visitor)
+        checking_visitor = CheckingVisitor.new
+        checking_visitor.checks = @checks
+        iterator_visitor = IteratorVisitor.new(checking_visitor)
+        node.accept(iterator_visitor)
       end
 
       def check_content(content)
@@ -51,8 +60,7 @@ module Roodi
       
       def load_checks
         check_objects = []
-        config_file = File.join(File.dirname(__FILE__), "..", "..", "..", "roodi.yml")
-        check_config = YAML.load_file config_file
+        check_config = YAML.load_file @config
         checks = check_config["checks"]
         checks.each { |check| check_objects << eval("Roodi::Checks::#{check['name']}.new") }
         check_objects
