c2aa79b0e114247fd88470225f012ad742163c01
Marty Andrews <marty@martyandrews.net>
2008-09-06 23:28:10 +1000

    MethodNameCheck working again.

--- lib/roodi/checks/method_name_check.rb
+++ lib/roodi/checks/method_name_check.rb
@@ -4,12 +4,12 @@ module Roodi
   module Checks
     class MethodNameCheck < Check
       def interesting_nodes
-        [:method]
+        [:defn]
       end
 
       def evaluate(node)
-          pattern = /^[a-z]+[a-z0-9_]*[!\?]?$/
-        add_error "#{position(node)} - Method name \"#{node.getName}\" should match pattern #{pattern}." unless node.getName =~ pattern
+        pattern = /^[a-z]+[a-z0-9_]*[!\?]?$/
+        add_error "#{position(node)} - Method name \"#{node[1]}\" should match pattern #{pattern}." unless node[1].to_s =~ pattern
       end
     end
   end
