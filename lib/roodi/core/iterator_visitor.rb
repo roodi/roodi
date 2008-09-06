f7c3b4c003fee06262b5ad957f3374eb1614e07d
Marty Andrews <marty@martyandrews.net>
2008-09-06 23:51:31 +1000

    removed magic number check.

--- lib/roodi/core/iterator_visitor.rb
+++ lib/roodi/core/iterator_visitor.rb
@@ -6,7 +6,6 @@ module Roodi
     	end
 	
     	def visit(visited)
-        # puts "Visiting #{visited.class} with value #{visited}"
     		visited.accept(@payload)
     		visitable_nodes = visited.is_language_node? ? visited.sexp_body : visited
     		visitable_nodes.each do |child| 
