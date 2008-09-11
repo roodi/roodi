2f525b0cadab5bd572d8dbce5aebdf97b46bc8a2
Marty Andrews <marty@martyandrews.net>
2008-09-11 21:51:19 +1000

    moved specs to match lib structure.

--- spec/roodi/checks/method_line_count_check_spec.rb
+++ spec/roodi/checks/method_line_count_check_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe Roodi::Checks::MethodLineCountCheck do
   before(:each) do
