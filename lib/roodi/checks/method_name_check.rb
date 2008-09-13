5e15b5b2dccda1e40e6f17a5733ecdef048d8cd5
Marty Andrews <marty@martyandrews.net>
2008-09-13 14:18:02 +1000

    moved parameters to constructors in preparation for configuration via yaml.

--- lib/roodi/checks/method_name_check.rb
+++ lib/roodi/checks/method_name_check.rb
@@ -3,13 +3,18 @@ require 'roodi/checks/check'
 module Roodi
   module Checks
     class MethodNameCheck < Check
+      def initialize(pattern = /^[_a-z<>=\[\]|+-\/\*`]+[_a-z0-9_<>=~@\[\]]*[=!\?]?$/)
+        super()
+        @pattern = pattern
+      end
+
       def interesting_nodes
         [:defn]
       end
 
       def evaluate(node)
         pattern = /^[_a-z<>=\[\]|+-\/\*`]+[_a-z0-9_<>=~@\[\]]*[=!\?]?$/
-        add_error "Method name \"#{node[1]}\" should match pattern #{pattern.inspect}" unless node[1].to_s =~ pattern
+        add_error "Method name \"#{node[1]}\" should match pattern #{@pattern.inspect}" unless node[1].to_s =~ @pattern
       end
     end
   end
--- lib/roodi/checks/module_name_check.rb
+++ lib/roodi/checks/module_name_check.rb
@@ -3,14 +3,18 @@ require 'roodi/checks/check'
 module Roodi
   module Checks
     class ModuleNameCheck < Check
+      def initialize(pattern = /^[A-Z][a-zA-Z0-9]*$/)
+        super()
+        @pattern = pattern
+      end
+
       def interesting_nodes
         [:module]
       end
   
       def evaluate(node)
         class_name = node[1].class == Symbol ? node[1] : node[1].last
-        pattern = /^[A-Z][a-zA-Z0-9]*$/
-        add_error "Module name \"#{node[1]}\" should match pattern #{pattern.inspect}" unless class_name.to_s =~ pattern
+        add_error "Module name \"#{class_name}\" should match pattern #{@pattern.inspect}" unless class_name.to_s =~ @pattern
       end
     end
   end
