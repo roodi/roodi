874b2b2f946de7c541fd9de119ba0ed8845d12fa
Marty Andrews <marty@martyandrews.net>
2008-09-17 21:50:05 +1000

    Factored out duplication from line count checks.

---
+++ lib/roodi/checks/line_count_check.rb
@@ -0,0 +1,32 @@
+require 'roodi/checks/check'
+
+module Roodi
+  module Checks
+    class LineCountCheck < Check
+      def initialize(interesting_nodes, configured_line_count, message_prefix)
+        super()
+        @interesting_nodes = interesting_nodes
+        @line_count = configured_line_count || DEFAULT_LINE_COUNT
+        @message_prefix = message_prefix
+      end
+
+      def interesting_nodes
+        @interesting_nodes
+      end
+
+      def evaluate(node)
+        line_count = count_lines(node)
+        add_error "#{@message_prefix} \"#{node[1]}\" has #{line_count} lines.  It should have #{@line_count} or less." unless line_count <= @line_count
+      end
+
+      protected
+  
+      def count_lines(node)
+        count = 0
+        count = count + 1 if node.node_type == :newline
+        node.children.each {|node| count += count_lines(node)}
+        count
+      end
+    end
+  end
+end
