5a635219d3aaa739b888d62f75342cf058ac6dd6
Marty Andrews <marty@martyandrews.net>
2009-04-10 15:24:38 +1000

    Fixed bug in rake task.

--- lib/roodi.rb
+++ lib/roodi.rb
@@ -2,5 +2,5 @@ require 'roodi/checks'
 require 'roodi/core'
 
 module Roodi
-  VERSION = '1.3.4'
+  VERSION = '1.3.5'
 end
--- lib/roodi_task.rb
+++ lib/roodi_task.rb
@@ -4,15 +4,13 @@ class RoodiTask < Rake::TaskLib
   attr_accessor :config
   attr_accessor :verbose
 
-  def initialize name = :roodi, patterns = nil, config nil
+  def initialize name = :roodi, patterns = nil, config = nil
     @name      = name
     @patterns  = patterns || %w(app/**/*.rb lib/**/*.rb spec/**/*.rb test/**/*.rb)
     @config    = config
     @verbose   = Rake.application.options.trace
 
     yield self if block_given?
-
-    @dirs.reject! { |f| ! File.directory? f }
 
     define
   end
