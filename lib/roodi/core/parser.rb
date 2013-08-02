e542cb9d831c10bf1bb17396234d7100b0d9b853
Martin Gotink <m.gotink@gmail.com>
2013-08-02 18:28:30 +0200

    Refactor getting output stream

--- lib/roodi/core/parser.rb
+++ lib/roodi/core/parser.rb
@@ -15,8 +15,7 @@ module Roodi
       
       def silence_stream(stream)
         old_stream = stream.dup
-        is_windows = !!(RbConfig::CONFIG['host_os'] =~ /mingw|mswin32|cygwin/)
-        stream.reopen(is_windows ? 'NUL:' : '/dev/null')
+        stream.reopen(null_stream_output)
         stream.sync = true
         yield
       ensure
@@ -26,6 +25,23 @@ module Roodi
       def silent_parse(content, filename)
         @parser ||= RubyParser.new
         @parser.parse(content, filename)
+      end
+      
+      def null_stream_output
+        null_output_for_platform(RbConfig::CONFIG['host_os'])
+      end
+      
+      def null_output_for_platform(host_os)
+        case host_os
+        when windows_host_os_matcher
+          'NUL:'
+        else
+          '/dev/null'
+        end
+      end
+      
+      def windows_host_os_matcher
+        /mingw|mswin32|cygwin/o
       end
     end
   end
