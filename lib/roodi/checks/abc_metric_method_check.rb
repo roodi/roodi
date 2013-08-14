4b2bd56fe6a17bb2fbc6d1ca44451dbbbf825533
Peter Evjan <peter.evjan@gmail.com>
2013-08-14 18:10:57 +1000

    Whitespace corrections

--- lib/roodi/checks/abc_metric_method_check.rb
+++ lib/roodi/checks/abc_metric_method_check.rb
@@ -3,7 +3,7 @@ require 'roodi/checks/check'
 module Roodi
   module Checks
     # TODO:  Add summary
-    # 
+    #
     # TODO:  Add detail
     class AbcMetricMethodCheck < Check
       # ASSIGNMENTS = [:attrasgn, :attrset, :dasgn_curr, :iasgn, :lasgn, :masgn]
@@ -15,14 +15,14 @@ module Roodi
       #  =  *=  /=  %=  +=  <<=  >>=  &=  |=  ^=
       OPERATORS = [:*, :/, :%, :+, :<<, :>>, :&, :|, :^]
       DEFAULT_SCORE = 10
-      
+
       attr_accessor :score
 
       def initialize
         super()
         self.score = DEFAULT_SCORE
       end
-      
+
       def interesting_nodes
         [:defn]
       end
@@ -35,7 +35,7 @@ module Roodi
         score = Math.sqrt(a*a + b*b + c*c)
         add_error "Method name \"#{method_name}\" has an ABC metric score of <#{a},#{b},#{c}> = #{score}.  It should be #{@score} or less." unless score <= @score
       end
-      
+
       private
 
       def count_assignments(node)
@@ -44,7 +44,7 @@ module Roodi
         node.children.each {|node| count += count_assignments(node)}
         count
       end
-      
+
       def count_branches(node)
         count = 0
         count = count + 1 if branch?(node)
@@ -58,22 +58,22 @@ module Roodi
         node.children.each {|node| count += count_conditionals(node)}
         count
       end
-      
+
       def assignment?(node)
         ASSIGNMENTS.include?(node.node_type)
       end
-      
+
       def branch?(node)
         BRANCHES.include?(node.node_type) && !conditional?(node) && !operator?(node)
       end
-      
+
       def conditional?(node)
-        (:call == node.node_type) && CONDITIONS.include?(node[2]) 
+        (:call == node.node_type) && CONDITIONS.include?(node[2])
       end
-      
+
       def operator?(node)
-        (:call == node.node_type) && OPERATORS.include?(node[2]) 
-      end      
+        (:call == node.node_type) && OPERATORS.include?(node[2])
+      end
     end
   end
 end
