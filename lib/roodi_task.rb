dc17b2c983afd12051a47402097bcd9665fb1e88
Peter Evjan <hello@peterevjan.com>
2013-11-09 16:09:17 +1100

    Added coloured output when running rake task
    Working towards solving #20

--- lib/roodi_task.rb
+++ lib/roodi_task.rb
@@ -29,12 +29,20 @@ class RoodiTask < Rake::TaskLib
 
       runner.start(@patterns)
 
-      runner.errors.each {|error| puts error}
-
-      puts "\nChecked #{runner.files_checked} files"
-
-      raise "Found #{runner.errors.size} errors." unless runner.errors.empty?
+      output_result(runner.errors, runner.files_checked)
     end
     self
+  end
+
+  def output_result(errors, files_checked)
+    errors.each {|error| puts "\e[31m#{error}\e[0m"}
+
+    puts "\nChecked #{files_checked} files"
+    result = "Found #{errors.size} errors."
+    if errors.empty?
+      puts "\e[32m#{result}\e[0m"
+    else
+      raise "\e[31m#{result}\e[0m"
+    end
   end
 end
